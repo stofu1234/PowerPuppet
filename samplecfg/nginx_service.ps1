4ebe10078da319e3cd712c10f48fa189a687f706
stofu1234 <yamaboku@gmail.com>
2015-10-31 23:35:18 +0900

    親プロセスキル時再起動 子プロセスキルは未実装

--- samplecfg/nginx_service.ps1
+++ samplecfg/nginx_service.ps1
@@ -26,32 +26,52 @@ function OnStart($args){
    "$(timeStamp) Script is starting..." >>$logPath
    cd $scriptPath
    Start-Process -FilePath $nginxPath 2>&1 >>$logPath
+
+   $start = Register-ObjectEvent -InputObject      $timer `
+                                 -SourceIdentifier TimerElapsed `
+                                 -EventName        Elapsed `
+                                 -Action           $watcher.GetNewClosure()
+   $timer.start()
+   
    "$(timeStamp) Done." >>$logPath
 }
 function OnStop(){
    "$(timeStamp) Script is stopping..." >>$logPath
    cd $scriptPath
    Start-Process -FilePath $nginxPath -Wait -ArgumentList @("-s","quit") 2>&1 >>$logPath
+   Unregister-Event TimerElapsed
    "$(timeStamp) Done." >>$logPath
 }
 function timeStamp{
    Get-Date -Format "yyyy/MM/dd hh:mm:ss.ff"
 }
-
-# 定数
-$scriptPath = Split-Path $script:myInvocation.MyCommand.path -parent
-$nginxPath  = Join-Path  $scriptPath "nginx.exe"           #　nginx.exeは本スクリプトと同一フォルダに配置
-$logPath    = Join-Path  $scriptPath "nginx_service.log"
-$pidPath    = Join-Path  $scriptPath "logs\nginx.pid"
-
+$watcher = {
+   #Write-Host "$(timeStamp) watching..."
+   "$(timeStamp) watching..." >>$logPath
+   $pid = cat $pidPath
+   $parentProcess = Get-Process -Id $pid -ea SilentlyContinue
+   if(! $?){
+      "$(timeStamp) Process[$pid] Notfound. Rebooting..." >>$logPath
+      OnStop
+      OnStart $null
+      "$(timeStamp) Done." >>$logPath
+   }
+   "$(timeStamp) Done." >>$logPath
+}
 # サービス設定情報
 $ServiceName = "nginx"
 $CanStop     = $true
 
+# 定数
+$scriptPath = Split-Path $script:myInvocation.MyCommand.path -parent
+$nginxPath  = Join-Path  $scriptPath "nginx.exe"
+$logPath    = Join-Path  $scriptPath "nginx_service.log"
+$pidPath    = Join-Path  $scriptPath "logs\nginx.pid"
+
+$timer = New-Object System.Timers.Timer
+$timer.Interval  = 60e3
 
 # 使用例　表示
 "Usage: InstallUtil.exe [<service>.exe]"
-
-
 
 
